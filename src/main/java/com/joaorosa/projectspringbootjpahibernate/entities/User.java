6b896759f1a4274b88196392ddafd0df5d645527
Joao Rosa <joao.rosa@gec.inatel.br>
2021-11-27 18:22:06 -0300

    H2 database, test profile, JPA

--- src/main/java/com/joaorosa/projectspringbootjpahibernate/entities/User.java
+++ src/main/java/com/joaorosa/projectspringbootjpahibernate/entities/User.java
@@ -1,7 +1,18 @@
 package com.joaorosa.projectspringbootjpahibernate.entities;
 
-public class User {
+import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
