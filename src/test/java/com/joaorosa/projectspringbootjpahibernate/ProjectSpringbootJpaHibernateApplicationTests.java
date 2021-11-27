19b036a60e328c58c3e30af5d5818485f26b8ebf
Joao Rosa <joao.rosa@gec.inate.br>
2021-11-27 10:35:54 -0300

    Project created

---
+++ src/test/java/com/joaorosa/projectspringbootjpahibernate/ProjectSpringbootJpaHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaorosa.projectspringbootjpahibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSpringbootJpaHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
