0497d3152e05c79c84293aeea9db82df2d70ca33
Joao Rosa <joao.rosa@gec.inatel.br>
2021-11-29 08:52:46 -0300

    Service layer, component registration

---
+++ src/main/java/com/joaorosa/springbootjpahibernate/services/UserService.java
@@ -0,0 +1,27 @@
+package com.joaorosa.springbootjpahibernate.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.joaorosa.springbootjpahibernate.entities.User;
+import com.joaorosa.springbootjpahibernate.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+
+	public List<User> findAll() {
+		return repository.findAll(); 
+	}
+
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
